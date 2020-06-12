1cbc868dd8cbbf08c38f7314c5081aa985bcd612
jleguy <jules.leguy@etud.univ-angers.fr>
2020-06-12 13:28:02 +0200

    install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EvoMol',
+version='1.0',
+description='Optimizing molecular properties with an evolutionary algorithm',
+url='https://github.com/jules-leguy/EvoMol',
+author='Jules Leguy',
+author_email='jules.leguy@univ-angers.fr',
+license='MIT',
+packages=['evomol'],
+zip_safe=False)
