6694a7909fc1330ac8c5b6426f7a22e5a2083167
jleguy <jules.leguy@etud.univ-angers.fr>
2021-02-06 15:37:43 +0100

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='EvoMol',
-version='1.0',
+version='1.1',
 description='Optimizing molecular properties with an evolutionary algorithm',
 url='https://github.com/jules-leguy/EvoMol',
 author='Jules Leguy',
