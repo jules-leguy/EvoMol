885590fbdcca8ebb0a3b095a4b15a147b5c7abe6
Jules Leguy <jules.leguy@etud.univ-angers.fr>
2022-02-08 18:30:28 +0100

    Remove test files

---
+++ evomol/test/35_test_cyclic_features.py
@@ -0,0 +1,6 @@
+from evomol.evaluation import UnknownGenericCyclicScaffolds
+
+extract = UnknownGenericCyclicScaffolds.extract_generic_cyclic_scaffolds
+
+print(extract(
+    "CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C"))
